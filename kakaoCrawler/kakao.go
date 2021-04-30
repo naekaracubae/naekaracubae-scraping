97b2e5c15efd7ed99562b59d3684d7d91f927e1e
msyhu <msyhu@korea.ac.kr>
2021-04-30 15:50:06 +0900

    - kakao 구조체 생성 - 페이지 수만큼 병렬로 크롤링

--- kakaoCrawler/kakao.go
+++ kakaoCrawler/kakao.go
@@ -10,6 +10,34 @@ import (
 
 var baseURL string = "https://careers.kakao.com/jobs?part=TECHNOLOGY&company=ALL"
 
+type ExtractedJob struct {
+	title     string
+	endDate   string
+	location  string
+	jobGroups []string
+	company   string
+	jobType   string
+}
+
+type extractedJob = ExtractedJob
+
+func Crawling(kakaoC chan<- []extractedJob) {
+	var jobs []extractedJob
+	c := make(chan []extractedJob)
+
+	totalPages := GetPages()
+	for i := 0; i < totalPages; i++ {
+		go GetPage(i, c)
+	}
+
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := <-c
+		jobs = append(jobs, extractedJobs...)
+	}
+
+	kakaoC <- jobs
+}
+
 // TODO : 페이지 끝까지 가서 가져와야 함.
 // 페이지 수를 가져온다
 func GetPages() int {
@@ -32,8 +60,9 @@ func GetPages() int {
 }
 
 // 하나의 페이지에서 직무를 가져와서 하나씩 채널로 넘겨준다.
-func GetPage(page int) {
-
+func GetPage(page int, mainC chan<- []extractedJob) {
+	var jobs []extractedJob
+	c := make(chan extractedJob)
 	pageURL := baseURL + "&page=" + strconv.Itoa(page)
 	fmt.Println(pageURL)
 	res, err := http.Get(pageURL)
@@ -48,44 +77,51 @@ func GetPage(page int) {
 	searchCards := doc.Find(".list_jobs>li")
 
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		extractJob(card)
+		go extractJob(card, c)
 	})
-	//
-	//for i := 0; i < searchCards.Length(); i++ {
-	//	job := <-c
-	//	jobs = append(jobs, job)
-	//}
-	//
-	//mainC <- jobs
+
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+
+	mainC <- jobs
 
 }
 
-func extractJob(card *goquery.Selection) {
+func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 	// title
 	title := card.Find(".tit_jobs").Text()
 
 	// endDate, location
 	var endDateAndLocation []string
 	card.Find(".list_info>dd").Each(func(i int, s *goquery.Selection) {
-		// I don't want the first string into my array, so I filter it
 		endDateAndLocation = append(endDateAndLocation, s.Text())
 	})
+
+	var endDate = ""
+	var location = ""
+	if len(endDateAndLocation) == 2 {
+		endDate = endDateAndLocation[0]
+		location = endDateAndLocation[1]
+	} else {
+		endDate = endDateAndLocation[0]
+	}
+
 	//jobGroups
 	var jobGroups []string
 	card.Find(".list_tag>a").Each(func(i int, s *goquery.Selection) {
-		// I don't want the first string into my array, so I filter it
 		jobGroup, _ := s.Attr("data-code")
 		jobGroups = append(jobGroups, jobGroup)
 	})
 	//company, jobType
 	var companyAndJobType []string
 	card.Find(".item_subinfo>dd").Each(func(i int, s *goquery.Selection) {
-		// I don't want the first string into my array, so I filter it
 		companyAndJobType = append(companyAndJobType, s.Text())
 	})
+	company := companyAndJobType[0]
+	jobType := companyAndJobType[1]
 
-	//c <- extractedJob{id, title, company, location}
-
-	fmt.Println(title, endDateAndLocation, jobGroups, companyAndJobType)
+	c <- extractedJob{title, endDate, location, jobGroups, company, jobType}
 
 }
--- kakaoCrawler/kakao_test.go
+++ kakaoCrawler/kakao_test.go
@@ -1,9 +1,12 @@
 package kakaoCrawler_test
 
 import (
+	"fmt"
 	"github.com/msyhu/GobbyIsntFree/kakaoCrawler"
 	"testing"
 )
+
+type extractedJob = kakaoCrawler.ExtractedJob
 
 func TestGetPages(t *testing.T) {
 	pages := kakaoCrawler.GetPages()
@@ -14,6 +17,27 @@ func TestGetPages(t *testing.T) {
 }
 
 func TestGetPage(t *testing.T) {
-	kakaoCrawler.GetPage(1)
+	c := make(chan []extractedJob)
+	kakaoCrawler.GetPage(1, c)
+
+}
+
+func TestCrawling(t *testing.T) {
+	var jobs []extractedJob
+	c := make(chan []extractedJob)
+
+	totalPages := kakaoCrawler.GetPages()
+	for i := 0; i < totalPages; i++ {
+		go kakaoCrawler.GetPage(i, c)
+	}
+
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := <-c
+		jobs = append(jobs, extractedJobs...)
+	}
+
+	for _, job := range jobs {
+		fmt.Println(job)
+	}
 
 }
