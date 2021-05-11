680611b01e2ef53700db9eaabc6a8f2960ff4d2c
msyhu <msyhu@korea.ac.kr>
2021-05-11 23:08:58 +0900

    - sendmail 모듈화

--- etc/sendmail.go
+++ etc/sendmail.go
@@ -1,21 +1,42 @@
 package etc
 
 import (
+	"encoding/json"
 	"fmt"
 	"gopkg.in/gomail.v2"
+	"io/ioutil"
 )
 
-func SendMail(jobs []byte) {
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Name     string `json:"name"`
+}
+
+func SendMail(contents string) {
+	userJson, err := ioutil.ReadFile("./secrets/sendmail.json") // articles.json 파일의 내용을 읽어서 바이트 슬라이스에 저장
+	CheckErr(err)
+	var user User
+	json.Unmarshal(userJson, &user)
+
+	subscribersJson, err := ioutil.ReadFile("./secrets/subscribers.json") // articles.json 파일의 내용을 읽어서 바이트 슬라이스에 저장
+	CheckErr(err)
+	var subscribers []User
+	json.Unmarshal(subscribersJson, &subscribers)
+	fmt.Println(subscribers)
+
 	m := gomail.NewMessage()
-	m.SetHeader("From", "bogus92@kakao.com")
-	m.SetHeader("To", "bogus92@kakao.com")
-	m.SetAddressHeader("Cc", "bogus92@kakao.com", "Dan")
-	m.SetHeader("Subject", "Hello!")
-	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
+	m.SetHeader("From", user.Email)
+	var subscribersEmail []string
+	for _, subscriber := range subscribers {
+		subscribersEmail = append(subscribersEmail, subscriber.Email)
+	}
+	m.SetHeader("To", subscribersEmail...)
 
-	fmt.Println(string(jobs))
+	m.SetHeader("Subject", "Today's Kakao")
+	m.SetBody("text/html", contents)
 
-	d := gomail.NewDialer("smtp.kakao.com", 465, "bogus92@kakao.com", "zes200101")
+	d := gomail.NewDialer("smtp.kakao.com", 465, user.Email, user.Password)
 
 	// Send the email to Bob, Cora and Dan.
 	if err := d.DialAndSend(m); err != nil {
--- main.go
+++ main.go
@@ -5,19 +5,11 @@ import (
 	"fmt"
 	"github.com/msyhu/GobbyIsntFree/etc"
 	"github.com/msyhu/GobbyIsntFree/kakaoCrawler"
-	"gopkg.in/gomail.v2"
-	"io/ioutil"
 	"strconv"
 	"strings"
 )
 
 type kakaoExtractedJob = kakaoCrawler.ExtractedJob
-
-type User struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-}
 
 func main() {
 	kakaoC := make(chan []kakaoExtractedJob)
@@ -34,43 +26,7 @@ func main() {
 		contents.WriteString(idxString)
 		contents.WriteString("</br>")
 	}
-	//
-	//e, err := json.Marshal(&kakaoJobs)
-	//etc.CheckErr(err)
-	//contents += string(e)
 
-	//out, err := json.Marshal(kakaoJobs)
-	//etc.CheckErr(err)
-	// 메일 보내기
-	//etc.SendMail(out)
-
-	userJson, err := ioutil.ReadFile("./secrets/sendmail.json") // articles.json 파일의 내용을 읽어서 바이트 슬라이스에 저장
-	etc.CheckErr(err)
-	var user User
-	json.Unmarshal(userJson, &user)
-
-	subscribersJson, err := ioutil.ReadFile("./secrets/subscribers.json") // articles.json 파일의 내용을 읽어서 바이트 슬라이스에 저장
-	etc.CheckErr(err)
-	var subscribers []User
-	json.Unmarshal(subscribersJson, &subscribers)
-	fmt.Println(subscribers)
-
-	m := gomail.NewMessage()
-	m.SetHeader("From", user.Email)
-	var subscribersEmail []string
-	for _, subscriber := range subscribers {
-		subscribersEmail = append(subscribersEmail, subscriber.Email)
-	}
-	m.SetHeader("To", subscribersEmail...)
-
-	m.SetHeader("Subject", "Today's Kakao")
-	m.SetBody("text/html", contents.String())
-
-	d := gomail.NewDialer("smtp.kakao.com", 465, user.Email, user.Password)
-
-	// Send the email to Bob, Cora and Dan.
-	if err := d.DialAndSend(m); err != nil {
-		panic(err)
-	}
+	etc.SendMail(contents.String())
 
 }
