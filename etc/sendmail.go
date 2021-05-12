600e2dc3a6540ea0aacfab10d7b9a6a9ae71f6fd
msyhu <msyhu@korea.ac.kr>
2021-05-12 20:20:59 +0900

    - 메일 title별로 사용자 이름 적용

--- etc/sendmail.go
+++ etc/sendmail.go
@@ -14,12 +14,12 @@ type User struct {
 }
 
 func SendMail(contents string) {
-	userJson, err := ioutil.ReadFile("../secrets/sendmail.json") // articles.json 파일의 내용을 읽어서 바이트 슬라이스에 저장
+	userJson, err := ioutil.ReadFile("./secrets/sendmail.json") // articles.json 파일의 내용을 읽어서 바이트 슬라이스에 저장
 	CheckErr(err)
 	var user User
 	json.Unmarshal(userJson, &user)
 
-	subscribersJson, err := ioutil.ReadFile("../secrets/subscribers.json") // articles.json 파일의 내용을 읽어서 바이트 슬라이스에 저장
+	subscribersJson, err := ioutil.ReadFile("./secrets/subscribers.json") // articles.json 파일의 내용을 읽어서 바이트 슬라이스에 저장
 	CheckErr(err)
 	var subscribers []User
 	json.Unmarshal(subscribersJson, &subscribers)
@@ -27,20 +27,20 @@ func SendMail(contents string) {
 
 	m := gomail.NewMessage()
 	m.SetHeader("From", user.Email)
-	var subscribersEmail []string
-	for _, subscriber := range subscribers {
-		subscribersEmail = append(subscribersEmail, subscriber.Email)
-	}
-	m.SetHeader("To", subscribersEmail...)
-
-	// TODO : 사용자별로 이름 넣어주기
-	m.SetHeader("Subject", "Today's Kakao👶")
 	m.SetBody("text/html", contents)
-
 	d := gomail.NewDialer("smtp.kakao.com", 465, user.Email, user.Password)
-
-	// Send the email to Bob, Cora and Dan.
-	if err := d.DialAndSend(m); err != nil {
+	if _, err := d.Dial(); err != nil {
 		panic(err)
 	}
+
+	for _, subscriber := range subscribers {
+		m.SetHeader("To", subscriber.Email)
+		subject := subscriber.Name + " 님 ! 오늘의 채용정보입니다👶"
+		m.SetHeader("Subject", subject)
+
+		if err := d.DialAndSend(m); err != nil {
+			panic(err)
+		}
+	}
+
 }
